0d8971ce1de05c48164e8d354b4d31294672be95
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-02 17:06:44 +0500

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+// Function Prototype (Return type+fname+parameters)
+void printName();
+
+main()
+{  
+  //Function Call
+printName();
+}
+  //Function Defeination
+void printName()
+{
+   cout<<"I am patrick Bateman"<<endl;
+   cout<<"My roll no is 27"<<endl;
+   cout<<"MY aggregate is 94.3"<<endl;
+   cout<<"MY section is A"<<endl;
+   cout<<"I am patrick Bateman"<<endl;
+   cout<<"My roll no is 27"<<endl; 
+   cout<<"MY aggregate is 94.3"<<endl;
+   cout<<"MY section is A"<<endl;
+   cout<<"I am patrick Bateman"<<endl;
+   cout<<"My roll no is 27"<<endl;
+   cout<<"MY aggregate is 94.3"<<endl;
+   cout<<"MY section is A"<<endl;
+   
+}
+ 
